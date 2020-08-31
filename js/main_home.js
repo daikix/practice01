d99d815215323f3b00dae9e568c65a215ffaa2f0
Daiki <67086942+daikix@users.noreply.github.com>
2020-08-31 17:03:20 +0900

    Update main_home.js

--- js/main_home.js
+++ js/main_home.js
@@ -1,9 +1,16 @@
 let history = document.getElementById("history");
 let nowjob = document.getElementById("now-job");
 let hobby = document.getElementById("hobby");
+let back = document.getElementById("hobby_back");
+let body = document.body;
 history.addEventListener('click',popup,false);
 nowjob.addEventListener('click',popup,false);
-hobby.addEventListener('click',popup,false);
+hobby.addEventListener('click',function(){
+  body.classList.toggle("open");
+},false);
+back.addEventListener('click',function(){
+  body.classList.remove("open");
+},false);
 
 function popup(){
   alert("開発中...")
